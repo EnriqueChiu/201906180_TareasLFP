863955fb77ca1baf3b338acdfe6bd8eea965fa04
EnriqueChiu <68959257+EnriqueChiu@users.noreply.github.com>
2020-08-20 09:41:41 -0600

    Update Registro3.py

--- Tarea 2/Registro3.py
+++ Tarea 2/Registro3.py
@@ -8,4 +8,6 @@ with open('Registro3.csv') as File:
     for info in aux:
        print("REGISTRO: {0}, TIPO: {1}, RAZA: {2}, COLOR: {3}, TAMAÑO: {4}".format(info[0], info[1], info[2], info[3], info[4]))
        print(input("Click Enter"))
+        
+print(type(aux))
 print(input("Click Enter para Finalizar"))
